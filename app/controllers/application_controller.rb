899d18f883fc67e61c02c6c2795fcc138a053290
makaroni0416 <76754228+makaroni0416@users.noreply.github.com>
2021-03-26 21:11:52 +0900

    コントローラーの記述修正　ビューファイルのカラム記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[name,birthday,family_name,first_name,family_name_kana,first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name,:birthday,:family_name,:first_name,:family_name_kana,:first_name_kana])
   end
 end
 
