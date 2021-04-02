36cd9324d468fb6f86c31400b4900dfad6016fa0
makaroni0416 <76754228+makaroni0416@users.noreply.github.com>
2021-04-02 19:35:27 +0900

    バリデーションの修正、エラー文表示の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :name
+    validates :birthday
+  end
 
-  validates :name, presence: true
-  validates :birthday, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+  end
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
