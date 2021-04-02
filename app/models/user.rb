ed14fac9ccd111b5db0f307ca9031c39ab50043f
makaroni0416 <76754228+makaroni0416@users.noreply.github.com>
2021-04-02 19:56:05 +0900

    全角カタカナのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナ文字を使用してください' } do  
     validates :family_name_kana
     validates :first_name_kana
   end
